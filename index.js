edb45dc09744757e7bf52f298700c5337e424fd4
Agustin Carthery <acarthery@Agustins-MacBook-Pro.local>
2021-11-05 17:30:06 -0300

    BaSD-MS-W8-001: Setup npm and express

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hi")
+const express = require('express')
+const admins = require('./controllers/admins')
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('App MindSet already running')
+})
+
+app.listen(port, () => {
+  console.log(`App MindSet listening at http://localhost:${port}`)
+})
