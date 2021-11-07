eda7b426f34ab42b12a8efbca453b275c88cec3e
ortizjoaquin <joaquin.ortizdlc@gmail.com>
2021-11-07 16:25:38 -0300

    fixed indentation details

--- controllers/psychologists.js
+++ controllers/psychologists.js
@@ -1,14 +1,17 @@
-const fs = require('fs')
-const Psychologists = require('../data/psychologists.json')
+// Variables
+const fs = require('fs');
+const Psychologists = require('../data/psychologists.json');
+
+// Code shortened
 const getPsychologistIndex = (req) => {
-  return (Psychologists.findIndex((item) => item.id === req.query.id))
+  return (Psychologists.findIndex((item) => item.id === req.query.id));
 }
 const writeFile = (file, obj) => {
   fs.writeFile(file, JSON.stringify(obj), {}, (error) => {
     if (error) {
-      res.status(400).send(error)
-    }
-  })
+      res.status(400).send(error);
+    };
+  });
 }
 
 // Get All Psychologysts
@@ -20,11 +23,11 @@ const getPsychologists = (req, res) => {
 const getOnePsychologist = (req, res) => {
 	const psychologistIndex = getPsychologistIndex(req);
 	if (psychologistIndex != -1) {
-		res.status(200).json(Psychologists[psychologistIndex])
+		res.status(200).json(Psychologists[psychologistIndex]);
 	}
 	else {
-		res.status(400).json({ msg: `No psychologist with the Id of ${req.query.id}` })
-	}
+		res.status(400).json({ msg: `No psychologist with the Id of ${req.query.id}` });
+	};
 }
 
 // Create New Psychologyst
@@ -32,7 +35,7 @@ const createPsychologist = (req,res) => {
   if (Object.keys(req.query).length === 0){
     return res.status(400).send({
       msg: 'Missing all files'
-  });
+    });
   }
   if (!req.query?.first_name){
     return res.status(400).send({
@@ -42,22 +45,22 @@ const createPsychologist = (req,res) => {
   else if (!req.query?.last_name){
     return res.status(400).send({
       msg: 'The last name is missing'
-  });
+    });
   }
   else if (!req.query?.user_name){
     return res.status(400).send({
       msg: 'The user name is missing'
-  });
+    });
   }
   else if (!req.query?.email){
     return res.status(400).send({
       msg: 'The email is missing'
-  });
+    });
   }
   else if (!req.query?.password){
     return res.status(400).send({
       msg: 'The password is missing'
-  });
+    });
   }
 
   const {
@@ -81,7 +84,7 @@ const createPsychologist = (req,res) => {
   writeFile('./data/psychologists.json', Psychologists)
   return res.status(201).send({
     msg: 'Psychologist Created'
-  })
+  });
 }
 
 // Edit Existing Psychologist
@@ -95,17 +98,14 @@ const editPsychologist = (req, res) => {
 			user_name: req.query.user_name || Psychologists[psychologistIndex].user_name,
 			email: req.query.email || Psychologists[psychologistIndex].email,
 			password: req.query.password || Psychologists[psychologistIndex].password,
-		}
-		fs.writeFile('./data/psychologists.json', JSON.stringify(Psychologists), {}, (error) => {
-			if (error) {
-				res.status(400).send(error)
-			} else {
-				res.status(201).json(Psychologists[psychologistIndex])
-			}
-		});
+		};
+    writeFile('./data/psychologists.json', Psychologists)
+    return res.status(201).send({
+      msg: 'Psychologist Edited'
+    });
 	} else {
 		res.status(404).send('Psychologist not found');
-	}
+	};
 }
 
 // Delete Existing Psychologist
@@ -116,23 +116,20 @@ const deletePsychologist = (req, res) => {
 			msg: `Psychologist with id ${req.query.id} deleted`
 		});
 		Psychologists.splice(psychologistIndex, 1)
-		fs.writeFile('./data/psychologists.json', JSON.stringify(Psychologists), {}, (error) => {
-			if (error) {
-				res.status(400).send(error)
-			} else {
-				res.status(200).json(Psychologists[psychologistIndex])
-			}
-		});
+    writeFile('./data/psychologists.json', Psychologists)
+    return res.status(201).send({
+      msg: 'Psychologist Edited'
+    });
 	}
 	else {
 		res.status(404).send('Psychologist not found');
-	}
+	};
 }
 
 module.exports = {
 	getPsychologists,
 	getOnePsychologist,
 	editPsychologist,
-	deletePsychologist,
-  createPsychologist
+  createPsychologist,
+	deletePsychologist
 }
