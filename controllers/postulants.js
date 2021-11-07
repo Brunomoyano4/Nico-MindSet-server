8e3342545bc7625ac903f2c3010e19a9e6d4a5b0
Micaela Quarleri <micaquarleri@gmail.com>
2021-11-07 16:30:05 -0300

    BaSD-MS-W8-003:corrections made

--- controllers/postulants.js
+++ controllers/postulants.js
@@ -1,20 +1,17 @@
 const fs = require('fs')
 const Postulants = require('../data/postulants.json')
-
 const getPostulants = (req, res) => {
 	res.status(200).json(Postulants)
 }
-
 const getOnePostulant = (req, res) => {
 	const postulantIndex = Postulants.findIndex((item) => item.id === parseInt(req.query.id))
-	if (postulantIndex != -1) {
+	if (postulantIndex !== -1) {
 		res.status(200).json(Postulants[postulantIndex])
 	}
 	else {
 		res.status(400).json({ msg: `No postulant with the Id of ${req.query.id}` })
 	}
 }
-
 const editPostulants = (req, res) => {
 	const postulantIndex = Postulants.findIndex((item) => item.id === parseInt(req.query.id))
 	if (postulantIndex !== -1) {
@@ -25,13 +22,13 @@ const editPostulants = (req, res) => {
 			user_name: req.query.user_name || Postulants[postulantIndex].user_name,
 			email: req.query.email || Postulants[postulantIndex].email,
 			password: req.query.password || Postulants[postulantIndex].password,
-			birth_date:req.query.birth_date || Postulants[postulantIndex].birth_date,
-			street:req.query.street || Postulants[postulantIndex].street,
-			street_number:req.query.street_number || Postulants[postulantIndex].street_number,
-			city:req.query.city || Postulants[postulantIndex].city,
-			postal_code:req.query.postal_code || Postulants[postulantIndex].postal_code,
-			province_code:req.query.province || Postulants[postulantIndex].province,
-			country:req.query.country || Postulants[postulantIndex].country,
+			birth_date: req.query.birth_date || Postulants[postulantIndex].birth_date,
+			street: req.query.street || Postulants[postulantIndex].street,
+			street_number: req.query.street_number || Postulants[postulantIndex].street_number,
+			city: req.query.city || Postulants[postulantIndex].city,
+			postal_code: req.query.postal_code || Postulants[postulantIndex].postal_code,
+			province_code: req.query.province || Postulants[postulantIndex].province,
+			country: req.query.country || Postulants[postulantIndex].country,
 		}
 		fs.writeFile('./data/postulants.json', JSON.stringify(Postulants), {}, (error) => {
 			if (error) {
@@ -46,7 +43,6 @@ const editPostulants = (req, res) => {
 }
 
 const deletePostulants = (req, res) => {
-
 	const postulantIndex = Postulants.findIndex((item) => item.id === parseInt(req.query.id))
 	if (postulantIndex !== -1) {
 		res.status(202).send({
@@ -66,91 +62,97 @@ const deletePostulants = (req, res) => {
 	}
 }
 
-const createPostulant = (req, res) =>{
-	const postulantIndex=Postulants.length+1
-	const newPostulantIndex=postulantIndex.toString()
-	if(req.query.first_name==undefined){
+const createPostulant = (req, res) => {
+	const postulantId = ((Postulants[Postulants.length - 1].id) + 1).toString()
+	if (!req.query.first_name) {
 		res.status(400).send({
 			msg: `Please enter a name`
 		});
-	} 
-	else if(req.query.last_name==undefined){
+	}
+	else if (!req.query.last_name) {
 		res.status(400).send({
 			msg: `Please enter a last name`
 		});
 	}
-	else if(req.query.user_name==undefined){
+	else if (!req.query.user_name) {
 		res.status(400).send({
 			msg: `Please enter an user name`
 		});
 	}
-	else if(req.query.email==undefined){
+	else if (!req.query.email) {
 		res.status(400).send({
 			msg: `Please enter an email`
 		});
 	}
-	else if(req.query.password==undefined){
+	else if (!req.query.password) {
 		res.status(400).send({
 			msg: `Please enter a password`
 		});
 	}
-	else if(req.query.birth_date==undefined){
+	else if (!req.query.birth_date) {
 		res.status(400).send({
 			msg: `Please enter a birth_date`
 		});
 	}
-	else if(req.query.street==undefined || req.query.street_number==undefined ){
+	else if (!req.query.street || !req.query.street_number) {
 		res.status(400).send({
 			msg: `Please enter a street adress`
 		});
 	}
-	else if(req.query.city==undefined){
+	else if (!req.query.city) {
 		res.status(400).send({
 			msg: `Please enter a city`
 		});
 	}
-	else if(req.query.postal_code==undefined){
+	else if (!req.query.postal_code) {
 		res.status(400).send({
 			msg: `Please enter a postal_code`
 		});
 	}
-	else if(req.query.province==undefined){
+	else if (!req.query.province) {
 		res.status(400).send({
 			msg: `Please enter a province`
 		});
 	}
-	else if(req.query.country==undefined){
+	else if (!req.query.country) {
 		res.status(400).send({
 			msg: `Please enter a country`
 		});
 	}
-	else if(req.query.telephone==undefined){
+	else if (!req.query.telephone) {
 		res.status(400).send({
 			msg: `Please enter a telephone`
 		});
 	}
-	else{
-		const newPostulant={
-			id:parseInt(newPostulantIndex),
+	else {
+		const newPostulant = {
+			id: parseInt(postulantId),
 			first_name: req.query.first_name,
 			last_name: req.query.last_name,
 			user_name: req.query.user_name,
 			email: req.query.email,
 			password: req.query.password,
-			birth_date:req.query.birth_date,
-			street:req.query.street,
-			street_number:req.query.street_number,
-			city:req.query.city,
-			postal_code:req.query.postal_code,
-			province:req.query.province,
-			country:req.query.country,
-			telephone:req.query.telephone
+			birth_date: req.query.birth_date,
+			street: req.query.street,
+			street_number: req.query.street_number,
+			city: req.query.city,
+			postal_code: req.query.postal_code,
+			province: req.query.province,
+			country: req.query.country,
+			telephone: req.query.telephone
 		}
 		res.status(202).send({
-			msg: `Postulant with id ${newPostulantIndex} created`
+			msg: `Postulant with id ${postulantId} created`
 		});
 		Postulants.push(newPostulant)
-	}		
+		fs.writeFile('./data/postulants.json', JSON.stringify(Postulants), {}, (error) => {
+			if (error) {
+				res.status(400).send(error)
+			} else {
+				res.status(200).json(newPostulant)
+			}
+		});
+	}
 }
 
 module.exports = {
@@ -159,4 +161,4 @@ module.exports = {
 	editPostulants,
 	deletePostulants,
 	createPostulant
-} 
+}
