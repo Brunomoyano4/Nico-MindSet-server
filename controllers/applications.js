5dc9905d7e6f2c7da51a164b09af9cd679ab7808
veronabaudracco <veronabaudracco@gmail.com>
2021-11-08 10:35:55 -0300

    Fixed indentation in position

--- controllers/applications.js
+++ controllers/applications.js
@@ -2,86 +2,86 @@ const fs = require('fs')
 const Applications = require('../data/applications.json')
 
 const missingInputs = (req, res) => {
-    if (!req.query.postulantId && !req.query.positionId) {
-        return res.status(400).send({ msg: 'postulantId and positionId are empty' })
-    }
-    if (!req.query.postulantId) {
-        return res.status(400).send({ msg: 'postulantId is empty' })
-    }
-    if (!req.query.positionId) {
-        return res.status(400).send({ msg: 'positionId is empty' })
-    }
+  if (!req.query.postulantId && !req.query.positionId) {
+    return res.status(400).send({ msg: 'postulantId and positionId are empty' })
+  }
+  if (!req.query.postulantId) {
+    return res.status(400).send({ msg: 'postulantId is empty' })
+  }
+  if (!req.query.positionId) {
+    return res.status(400).send({ msg: 'positionId is empty' })
+  }
 }
 
 const findIndex = (req) => {
-    return Applications.findIndex((element) => element.positionId === req.query.positionId && element.postulantId === req.query.PostulantId)
+  return Applications.findIndex((element) => element.positionId === req.query.positionId && element.postulantId === req.query.PostulantId)
 }
 
 const filter = (req) => {
-    return Applications.filter((element) => element.positionId === req.query.positionId)
+  return Applications.filter((element) => element.positionId === req.query.positionId)
 }
 
 const getApplications = (req, res) => {
-    res.status(200).json(Applications)
+  res.status(200).json(Applications)
 }
 
 const getApplicationByPositionId = (req, res) => {
-    const applicationsByPositionId = filter(req)
-    if (!applicationsByPositionId) {
-        res.status(400).json({ msg: `No applications for the positionId ${req.query.positionId}` })
-    } else {
-        res.status(200).json(applicationsByPositionId)
-    }
+  const applicationsByPositionId = filter(req)
+  if (!applicationsByPositionId) {
+    res.status(400).json({ msg: `No applications for the positionId ${req.query.positionId}` })
+  } else {
+    res.status(200).json(applicationsByPositionId)
+  }
 }
 
 const getApplicationByPostulantId = (req, res) => {
-    const applicationsByPostulantId = filter(req)
-    if (!applicationsByPostulantId) {
-        res.status(400).json({ msg: `No applications for the positionId ${req.query.postulantId}` })
-    } else {
-        res.status(200).json(applicationsByPostulantId)
-    }
+  const applicationsByPostulantId = filter(req)
+  if (!applicationsByPostulantId) {
+    res.status(400).json({ msg: `No applications for the positionId ${req.query.postulantId}` })
+  } else {
+    res.status(200).json(applicationsByPostulantId)
+  }
 }
 
 const createApplication = (req, res) => {
-    missingInputs(req, res)
-    const newApplication = {
-        postulantId: req.query.postulantId,
-        positionId: req.query.positionId,
-        createdAt: Date.now().toString()
+  missingInputs(req, res)
+  const newApplication = {
+    postulantId: req.query.postulantId,
+    positionId: req.query.positionId,
+    createdAt: Date.now().toString()
+  }
+  Applications.push(newApplication)
+  fs.writeFile('./data/applications.json', JSON.stringify(Applications), {}, (error) => {
+    if (error) {
+      res.status(400).send(error)
+    } else {
+      res.status(200).json(newApplication)
     }
-    Applications.push(newApplication)
-    fs.writeFile('./data/applications.json', JSON.stringify(Applications), {}, (error) => {
-        if (error) {
-            res.status(400).send(error)
-        } else {
-            res.status(200).json(newApplication)
-        }
-    })
+  })
 }
 
 const deleteApplication = (req, res) => {
-    missingInputs(req, res)
-    const index = findIndex(req)
-    if (index) {
-        res.status(202).send({ msg: `Application with positionId ${req.query.positionId} and postulantId ${req.query.postulantId} deleted` })
-        Applications.splice(index, 1)
-        fs.writeFile('./data/applications.json', JSON.stringify(Applications), {}, (error) => {
-            if (error) {
-                res.status(400).send(error)
-            } else {
-                res.status(200).json(Applications[index])
-            }
-        })
-    } else {
-        res.status(404).json({ msg: `No application found with  positionId ${req.query.positionId} and postulantId ${req.query.postulantId}` });
-    }
+  missingInputs(req, res)
+  const index = findIndex(req)
+  if (index) {
+    res.status(202).send({ msg: `Application with positionId ${req.query.positionId} and postulantId ${req.query.postulantId} deleted` })
+    Applications.splice(index, 1)
+    fs.writeFile('./data/applications.json', JSON.stringify(Applications), {}, (error) => {
+      if (error) {
+        res.status(400).send(error)
+      } else {
+        res.status(200).json(Applications[index])
+      }
+    })
+  } else {
+    res.status(404).json({ msg: `No application found with  positionId ${req.query.positionId} and postulantId ${req.query.postulantId}` });
+  }
 }
 
 module.exports = {
-    getApplications,
-    getApplicationByPositionId,
-    getApplicationByPostulantId,
-    createApplication,
-    deleteApplication
+  getApplications,
+  getApplicationByPositionId,
+  getApplicationByPostulantId,
+  createApplication,
+  deleteApplication
 }
