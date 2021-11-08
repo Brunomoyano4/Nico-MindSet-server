c3629ee8dc8de0c46ef286460822825c3008cabf
Manuel Lopez <mlopez@liliana.com.ar>
2021-11-08 15:17:03 -0300

    BaSD-MS-W8-009-Interviews-resource: Index.js modifications added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const admins = require('./controllers/admins')
+const interviews = require('./controllers/interviews')
 
 const app = express()
 const port = 3000
@@ -14,6 +15,11 @@ app.get('/admin', admins.getOneAdmin)
 app.get('/admin/edit', admins.editAdmin)
 app.get('/admin/delete', admins.deleteAdmin)
 
+//Interviews resource 
+app.get('/interviews', interviews.getInterviews)
+app.get('/interview', interviews.getOneInterview)
+app.get('/interview/create', interviews.createInterview)
+app.get('/interviews/delete', interviews.deleteInterview)
 
 app.listen(port, () => {
   console.log(`App MindSet listening at http://localhost:${port}`)
