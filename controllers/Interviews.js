827c672ec5eb6fbf57942c0616bfaa709ba45332
Manuel Lopez <mlopez@liliana.com.ar>
2021-11-08 14:55:11 -0300

    BaSD-MS-W8-009: Modify index.js, create interviews.json, create interviews.js

---
+++ controllers/Interviews.js
@@ -0,0 +1,83 @@
+const fs = require('fs')
+const Interviews = require('../data/interviews.json')
+
+const getInterviews = (req, res) => {
+    res.status(200).json(Interviews)
+}
+
+const getOneInterview = (req, res) => {
+    const oneInterview = Interviews.findIndex((item) => item.id === req.query.id)
+    if (oneInterview != false) {
+        res.status(200).json(Interviews[oneInterview])
+    } else {
+        res.status(400).json({ msg: `No Interview found with the Id of ${req.query.interviewId}` })
+    }
+}
+
+pos=post=date=time=stat="";
+okey=true;
+const createInterview = (req, res) => {
+    if (!req.query.positionId) {
+        pos = 'positonId, '
+        okey= false
+    }
+    if (!req.query.postulantId) {
+        post='postulantId, '
+        okey=false
+    }
+    if (!req.query.dateId) {
+        date='date, '
+        okey=false
+    }
+    if (!req.query.timeId) {
+        time='time, '
+        okey=false
+    }
+    if (!req.query.statusId) {
+        stat='status, '
+        okey=false
+    }
+    if okey=false{ 
+        res.status(400).send({ msg: pos + post + date + time + stat + 'are missing'})
+    }
+    const newInterview = {
+        interviewId:  Object.keys(Interviews).length + 1 /*new Date().getTime().toString()*/,
+        positionId: req.query.postionId,
+        postulantId: req.query.postulantId,
+        date: req.query.date
+        time: req.query.time
+        stat: req.query.stat
+    }
+    Interviews.push(newInterview)
+    fs.writeFile('./data/interviews.json', JSON.stringify(Interviews), (error) => {
+        if (error) {
+            res.status(400).json({ msg: "Problems adding the new Interview" })
+        } else {
+            res.status(200).json(newInterview)
+        }
+    })
+}
+
+const deleteInterview = (req, res) => {
+    const oneInterview = Interviews.findIndex((item) => item.id === req.query.id)
+    if (oneInterview != False) {
+		res.status(202).send({ msg: "Interview with id ", ${req.query.id} ," deleted" })
+		Interviews.splice(Interviews, 1)
+		fs.writeFile('./data/interviews.json', JSON.stringify(Interviews), (error) => {
+			if (error) {
+                return res.status(400).json({ msg: "Problems deleting the Interview" })
+            } else {
+                return res.status(200).json(newInterview)
+            }
+		})
+	} else {
+		return res.status(400).json({ msg: `No interview with the id of ${req.query.id}` });
+	}
+}
+
+module.exports = {
+	getInterviews,
+    getOneInterview,
+    createInterview,
+    deleteInterview
+}
