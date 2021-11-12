fa46363f66522003a7a6a4bc70c5753fa8c31b1c
Agustin Carthery <acarthery@Agustins-MacBook-Pro.local>
2021-11-12 11:06:16 -0300

    BaSD-MS-W9-011: setting cluster connection

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
 const port = 3000
 
+app.use(express.json())
+
+mongoose.connect('mongodb+srv://MindSet:BaSD-MindSet2021@cluster0.rblv6.mongodb.net/mindSet?retryWrites=true&w=majority',
+  (error) => {
+    if (error) {
+      console.log('Error : ', error)
+    } else {
+      console.log('Mindset db connected')
+    }
+  }
+)
 
 app.listen(port, () => {
   console.log(`App MindSet listening at http://localhost:${port}`)
