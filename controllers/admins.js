560bddbe66b1759ae5c9fef448f197fd0f6621ea
Agustin Carthery <acarthery@Agustins-MacBook-Pro.local>
2021-11-12 14:11:54 -0300

    BaSD-MS-W9-013: Draft Admin controllers

--- controllers/admins.js
+++ controllers/admins.js
@@ -1,69 +1,113 @@
-const fs = require('fs')
-const Admins = require('../data/admins.json')
+const Admin = require('../models/Admins.js')
 
-const getAdmins = (req, res) => {
-	res.status(200).json(Admins)
-}
+exports.createAdmin = (req, res) => {
+  if (
+    !req.body?.first_name ||
+    !req.body?.last_name ||
+    !req.body?.user_name ||
+    !req.body?.email ||
+    !req.body?.password
+  ) {
+    return res.status(400).send({
+      msg: "There are missing inputs",
+    });
+  }
+  const admin = new Admin({
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    user_name: req.body.user_name,
+    email: req.body.email,
+    password: req.body.password,
+  });
 
-const getOneAdmin = (req, res) => {
-	const adminIndex = Admins.findIndex((item) => item.id === req.query.id)
-	if (adminIndex != -1) {
-		res.status(200).json(Admins[adminIndex])
-	}
-	else {
-		res.status(400).json({ msg: `No admin with the Id of ${req.query.id}` })
-	}
-}
-
-const editAdmin = (req, res) => {
-	const adminIndex = Admins.findIndex((item) => item.id === req.query.id)
-	if (adminIndex !== -1) {
-		Admins[adminIndex] = {
-			id: Admins[adminIndex].id,
-			first_name: req.query.first_name || Admins[adminIndex].first_name,
-			last_name: req.query.last_name || Admins[adminIndex].last_name,
-			user_name: req.query.user_name || Admins[adminIndex].user_name,
-			email: req.query.email || Admins[adminIndex].email,
-			password: req.query.password || Admins[adminIndex].password,
-		}
-		fs.writeFile('./data/admins.json', JSON.stringify(Admins), {}, (error) => {
-			if (error) {
-				res.status(400).send(error)
-			} else {
-				res.status(201).json(Admins[adminIndex])
-			}
-		})
-	} else {
-		res.status(404).send("Admin not found")
-	}
-}
-
-const deleteAdmin = (req, res) => {
-
-	const adminIndex = Admins.findIndex((item) => item.id === req.query.id)
-	if (adminIndex !== -1) {
-		res.status(202).send({
-			msg: `Admin with id ${req.query.id} deleted`
-		});
-		Admins.splice(adminIndex, 1)
-		fs.writeFile('./data/admins.json', JSON.stringify(Admins), {}, (error) => {
-			if (error) {
-				res.status(400).send(error)
-			} else {
-				res.status(200).json(Admins[adminIndex])
-			}
-		});
-	}
-	else {
-		res.status(400).json({ msg: `No admin with the id of ${req.query.id}` });
-	}
-}
+  admin
+    .save()
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((e) => {
+      res.status(500).send({
+        msg: e.message || "There was an error during admin creation",
+      });
+    });
+};
 
 
-module.exports = {
-	getAdmins,
-	getOneAdmin,
-	editAdmin,
-	deleteAdmin
+exports.findAllAdmins = (req, res) => {
+  Admin.find({})
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((e) => {
+      res.status(500).send({
+        msg: e.message || "There was an error while retrieving admins",
+      });
+    });
+};
 
-}
+
+exports.findOneAdmin = (req, res) => {
+  Admin.findOne({ id: req.params.id })
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({
+          msg: `Admin with id ${req.params.id} was not found`,
+        });
+      }
+      res.send(data);
+    })
+    .catch((e) => {
+      res.status(500).send({
+        msg: e.message || "There was an error while retrieving admins",
+      });
+    });
+};
+
+
+exports.updateAdmin = (req, res) => {
+  if (!req.body) {
+    return res.status(400).send({
+      msg: "Data to update can not be empty!",
+    });
+  }
+
+  Admin.findOneAndUpdate({ id: req.params.id }, req.body, {
+    new: true,
+  })
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot update admin with the id: ${id},admin was not found!`,
+        });
+      } else
+        res.send({
+          message: `Admin with the id: ${id} was update successfully.`,
+        });
+    })
+    .catch((e) => {
+      res.status(500).send({
+        msg: e.message || `Error updating the admin with id: ${id}`,
+      });
+    });
+};
+
+
+exports.deleteAdmin = (req, res) => {
+  Admin.findOneAndRemove({ id: req.params.id })
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({
+          msg: "Admin not found",
+        });
+      } else {
+        res.send({
+          msg: "Admin was deleted successfully",
+        });
+      }
+    })
+    .catch((e) => {
+      res.status(500).send({
+        msg: `Error while deleting admin with id ${id}`,
+      });
+    });
+};
