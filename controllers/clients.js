5473b1f9408d0dc2923aed5fd3f26384912bea29
Micaela Quarleri <micaquarleri@gmail.com>
2021-11-14 16:44:57 -0300

    feature/BaSD-MS-W9-015: client files from controllers and models folder updated

--- controllers/clients.js
+++ controllers/clients.js
@@ -1,5 +1,99 @@
 const fs = require('fs');
-const Clients = require('../data/clients.json');
+const Clients = require('../models/clients');
+
+const getClients = (req, res) => {
+  Clients.find()
+   .then((Clients)=>{
+    res.status(200).json(Clients)
+   })
+   .catch((error)=>{
+    res.status(400).json(error)
+   })
+}
+
+const getOneClient = (req, res) => {
+  Clients.findById(req.params.id,
+  (error,Clients) => {
+    if(!Clients) {
+      return res.status(404).json({
+        msg: `Client with id: ${req.params.id} was not found`
+      })
+    }
+    if(error) {
+      return res.status(400).json(error)
+    }
+    return res.status(200).json(Clients)
+  })
+}
+
+const updateClient = (req, res) => {
+  Clients.findByIdAndUpdate(req.params.id,
+    { 
+      customer_name: req.body.customer_name,
+      branch: req.body.branch,
+      phone: req.body.phone,
+      email:req.body.email,
+      description:req.body.description
+    },
+    { new: true }, 
+    (error, newClient) => {
+      if(!newClient) {
+        return res.status(404).json({
+          msg: `Client with id: ${req.params.id} was not found`
+        })
+      }
+      if(error) {
+        return res.status(400).json(error)
+      }
+      return res.status(200).json(newClient)
+    }
+  )
+}
+
+const createClient=(req, res) => {
+  const client = new Clients({
+    customer_name: req.body.customer_name,
+    branch: req.body.branch,
+    phone: req.body.phone,
+    email:req.body.email,
+    description:req.body.description
+  })
+
+  client.save((error, client) => {
+    if(error) {
+      return res.status(400).json(error)
+    }
+    return res.status(201).json(client)
+  })
+}
+
+const deleteClient=(req,res)=>{
+  Clients.findByIdAndRemove(req.params.id,
+  (error,Clients) => {
+    if(!Clients) {
+      return res.status(404).json({
+        msg: `Client with id: ${req.params.id} was not found`
+      })
+    }
+    if(error) {
+      return res.status(400).json(error)
+    }
+    return res.status(200).json(Clients)
+  })
+}
+
+module.exports = {
+  getClients,
+  getOneClient,
+  createClient,
+  updateClient,
+  deleteClient
+}
+
+
+
+
+
 
 const writeJson = (file, obj, res, msg, code) => {
   fs.writeFile(file, JSON.stringify(obj), {}, (error) => {
