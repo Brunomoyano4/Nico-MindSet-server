22aed8c58b2b210d88d772b7012303e766b3b5fb
ortizjoaquin <joaquin.ortizdlc@gmail.com>
2021-11-15 17:24:50 -0300

    BaSD-MS-W9-019: added postman collection, cleaned up residual code

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -1,10 +1,6 @@
 const fs = require('fs')
 const Sessions = require('../models/Sessions')
 
-// const findIndex = (req) => {
-//   return sessions.findIndex((item) => item.id === req.query.id)
-// }
-
 const getSessions = (req, res) => {
   Sessions.find()
   .then ((sessions) => {
@@ -24,13 +20,6 @@ const getOneSession = (req, res) => {
     return res.status(400).json({ msg: `No session found with the Id of ${req.params._id}` })
   })
 }
-//   const index = findIndex(req) 
-//   if (index !== -1) {
-//     res.status(200).json(sessions[index])
-//   } else {
-//     res.status(400).json({ msg: `No session found with the Id of ${req.query.id}` })
-//   }
-// }
 
 const createSession = (req, res) => {
   const newSession = new Sessions({
@@ -48,25 +37,6 @@ const createSession = (req, res) => {
   })
 };
 
-//   missingInputs (req,res)
-//   const newSession = {
-//     id: (parseInt(sessions[sessions.length - 1].id) + 1).toString(),
-//     psychologyId: req.query.psychologyId,
-//     postulantId: req.query.postulantId,
-//     date: req.query.date,
-//     time: req.query.time,
-//     stat: req.query.stat
-//   }
-//   sessions.push(newSession)
-//   fs.writeFile('./data/sessions.json', JSON.stringify(sessions), (error) => {
-//     if (error) {
-//       res.status(400).json({ msg: "Problems adding the new session" })
-//     } else {
-//       res.status(200).json(newSession)
-//     }
-//   })
-// }
-
 const deleteSession = (req, res) => {
   Sessions.findByIdAndDelete({ _id: req.params._id })
   .then (() => {
@@ -76,25 +46,6 @@ const deleteSession = (req, res) => {
     return res.status(400).json({ msg: "Problems deleting the session" })
   })
 }
-//   const index = findIndex(req)  
-//   if (index !== -1) {
-// 		res.status(202).send({ msg: "Session with id " + req.query.id + " deleted" })
-//     const deletedElement = {
-//       ...sessions[index]
-//     }
-// 		sessions.splice(index, 1)
-// 		fs.writeFile('./data/sessions.json', JSON.stringify(sessions), (error) => {
-//       if (error) {
-//         res.status(400).json({ msg: "Problems deleting the session" })
-//       } else {
-//         res.status(200).json(deletedElement)
-//       }
-// 		})
-// 	}
-//   else {
-// 		res.status(400).json({ msg: `No session with the id of ${req.query.id}` });
-// 	}
-// }
 
 module.exports = {
   getSessions,
