990e05ddbeb4327e2f288cf306073170e34594cd
ortizjoaquin <joaquin.ortizdlc@gmail.com>
2021-11-22 21:28:48 -0300

    BaSD-MS-W10-023:done create, modify and delete endpoints

--- public/js/positionsForm.js
+++ public/js/positionsForm.js
@@ -1,3 +1,96 @@
 window.onload = () => {
   console.log('all set up ✅')
+
+  const navButton = document.getElementById('positionsNav');
+  navButton.classList.add('activePage');
+
+  const clientIdInput = document.getElementById('clientId');
+  const jobInput = document.getElementById('job');
+  const descriptionInput = document.getElementById('description');
+  const createdAtInput = document.getElementById('createdAt');
+
+  const form = document.getElementById('form');
+  const saveButton = document.getElementById('saveButton');
+  const errorMessage = document.getElementById('errorMessage');
+
+  const params = new URLSearchParams(window.location.search);
+  saveButton.disables = !!params.get('_id');
+
+  const onFocusInput = () => {
+    errorMessage.innerText = '';
+  };
+  
+  clientIdInput.onfocus = onFocusInput;
+  jobInput.onfocus = onFocusInput;
+  descriptionInput.onfocus = onFocusInput;
+  createdAtInput.onfocus = onFocusInput;
+
+  if (params.get('_id')) {
+    fetch(`${window.location.origin}/api/positions?_id=${params.get('_id')}`)
+      .then((response) => {
+        if (response.status !== 200) {
+          return response.json().then(({ message }) => {
+            throw new Error(message);
+          });
+        }
+        return response.json();
+      })
+      .then((response) => {
+        saveButton.disabled = false;
+        response.forEach((positions) => {
+          clientIdInput.value = positions.clientId
+          jobInput.value = positions.job
+          descriptionInput.value = positions.description
+          createdAtInput.value = positions.createdAt
+        });
+      })
+      .catch((error) => {
+        errorMessage.innerText = error;
+      });
+  };
+
+  form.onsubmit = (event) => {
+    event.preventDefault();
+    saveButton.disabled = true;
+
+    let url;
+    const options = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        clientId: clientIdInput.value,
+        job: jobInput.value,
+        description: descriptionInput.value,
+        createdAt: createdAtInput.value,
+      }),
+    };
+
+    if (params.get('_id')) {
+      options.method = 'PUT';
+      url = `${window.location.origin}/api/positions/${params.get('_id')}`;
+    } else {
+      options.method = 'POST';
+      url = `${window.location.origin}/api/positions`;
+    }
+
+    fetch(url, options)
+      .then((response) => {
+        if (response.status !==200 && response.status!==201) {
+          return response.json().then(({ message }) => {
+            throw new Error(message);
+          });
+        }
+        return response.json();
+      })
+      .then(() => {
+        window.location.href = `${window.location.origin}/views/positionsList.html`;
+      })
+      .catch((error) => {
+        errorMessage.innerText = error;
+      })
+      .finally(() => {
+        saveButton.disabled = false;
+      })
+  };
 };
--- public/js/positionsList.js
+++ public/js/positionsList.js
@@ -1,3 +1,84 @@
+const editPosition = (item) => {
+  window.location.href = `${window.location.origin}/views/positionsForm.html?_id=${item._id}`
+};
+
+const addNewPosition = () => {
+  window.location.href = `${window.location.origin}/views/positionsForm.html`
+}
+
+const deletePosition = (item) => {
+  const url = `${window.location.origin}/api/positions/${item._id}`;
+  fetch(url, {
+    method: 'DELETE',
+    headers: {
+      'Content-type': 'application/json',
+    },
+  })
+  .then((response) => {
+    if (response.status !== 200) {
+      return response.json().then(({ message }) => {
+        throw new Error(message);
+      });
+    }
+    return response.json();
+  })
+    .then(window.location.reload())
+    .catch((error) => error);
+};
+
 window.onload = () => {
-  console.log('all set up ✅')
+  const navButton = document.getElementById('positionsNav');
+  navButton.classList.add('activePage');
+  const tableContent = document.getElementById('positions-table');
+  const addPosition = document.getElementById('addPosition');
+  addPosition.onclick = addNewPosition;
+  fetch(`${window.location.origin}/api/positions`)
+    .then((response) => response.json())
+    .then((response) => {
+      response.forEach((item) => {
+        const tr = document.createElement('tr');
+        const clientTd = document.createElement('td');
+        const jobNameTd = document.createElement('td');
+        const jobDescriptionTd = document.createElement('td');
+        const createdAtTd = document.createElement('td');
+        const actionsTd = document.createElement('td');
+        clientTd.innerText = item.clientId;
+        jobNameTd.innerText = item.job;
+        jobDescriptionTd.innerText = item.description;
+        createdAtTd.innerText = item.createdAt;
+        
+        const button = document.createElement('button');
+        button.innerText = 'Delete';
+        actionsTd.append(button);
+        button.onclick = (event) => {
+          event.stopPropagation(event);
+          const modal=document.getElementById('modalPositions')
+          modal.classList.add('modal')
+          const textTitle=document.createElement('p')
+          const textModal=document.createElement('p')
+          textTitle.innerText='CAUTION:'
+          textTitle.classList.add('title')
+          textModal.innerText='Are you sure you want to delete this position?'
+          const buttonConfirm=document.createElement('button')
+          const buttonCancel=document.createElement('button')
+          buttonConfirm.innerText="Confirm"
+          buttonCancel.innerText="Cancel"
+          modal.append(textTitle, textModal, buttonCancel, buttonConfirm)
+          buttonConfirm.classList.add('modalButton')
+          buttonCancel.classList.add('modalButton')
+          buttonConfirm.onclick = () => deletePosition(item)
+          buttonCancel.onclick = () => {
+            modal.classList.add('modalRemove')
+            window.location.href = `${window.location.origin}/views/positionsList.html`;
+          };
+        };
+
+        tr.onclick = () => editPosition(item);
+        tr.append(clientTd, jobNameTd, jobDescriptionTd, createdAtTd, actionsTd);
+        tableContent.append(tr);
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 };
