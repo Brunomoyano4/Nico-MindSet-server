6e6e301a789f4e65bb7510af1601ed10723652b9
Martin Raveglia <ravegliamartin@gmail.com>
2022-01-03 15:35:36 -0300

    MS-88: Add psychologist user register

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,8 +4,14 @@ const validations = require('../validations/auth');
 
 const router = express.Router();
 
-const { register } = controller;
+const { registerPostulant, registerPsychologist, registerAdmin } = controller;
 
-router.post('/register', validations.required, register);
+router.post('/register', validations.required, registerPostulant);
+router.post(
+  '/register/psychologist',
+  validations.required,
+  registerPsychologist
+);
+router.post('/register/admin', validations.required, registerAdmin);
 
 module.exports = router;
