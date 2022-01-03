26088c9352240d5699f6762f86148a730ba1166e
Martin Raveglia <ravegliamartin@gmail.com>
2022-01-03 15:37:47 -0300

    MS-88: Add admin user register

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,8 +1,10 @@
 const Users = require('../models/Users');
 const Postulants = require('../models/Postulants');
+const Psychologists = require('../models/Psychologists');
+const Admins = require('../models/Admins');
 const Firebase = require('../helper/firebase');
 
-const register = async (req, res) => {
+const registerPostulant = async (req, res) => {
   let savedNewPostulant;
   try {
     const { email, password, userName: displayName } = req.body;
@@ -58,6 +60,96 @@ const register = async (req, res) => {
   }
 };
 
+const registerPsychologist = async (req, res) => {
+  let savedNewPsychologist;
+  try {
+    const { email, password, userName: displayName } = req.body;
+    const newPsychologist = new Psychologists({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      userName: req.body.userName,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    savedNewPsychologist = await newPsychologist.save();
+
+    const newFirebaseUser = await Firebase.auth().createUser({
+      displayName,
+      email,
+      password,
+    });
+    await Firebase.auth().setCustomUserClaims(newFirebaseUser.uid, {
+      role: 'psychologist',
+      mongoDBID: savedNewPsychologist._id,
+    });
+
+    const userCreated = new Users({
+      email,
+      role: 'psychologist',
+      firebaseUID: newFirebaseUser.uid,
+      mongoDBID: savedNewPsychologist._id,
+    });
+    const { firebaseUID } = await userCreated.save();
+
+    return res.status(201).json({
+      message: 'User created',
+      data: { firebaseUID, userData: savedNewPsychologist },
+    });
+  } catch (error) {
+    Psychologists.findOneAndRemove(
+      { _id: savedNewPsychologist._id },
+      { useFindAndModify: false },
+      () => res.status(400).json({ message: error.toString() })
+    );
+  }
+};
+
+const registerAdmin = async (req, res) => {
+  let savedNewAdmin;
+  try {
+    const { email, password, userName: displayName } = req.body;
+    const newAdmin = new Admin({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    savedNewAdmin = await newAdmin.save();
+
+    const newFirebaseUser = await Firebase.auth().createUser({
+      displayName,
+      email,
+      password,
+    });
+    await Firebase.auth().setCustomUserClaims(newFirebaseUser.uid, {
+      role: 'admin',
+      mongoDBID: savedNewAdmin._id,
+    });
+
+    const userCreated = new Users({
+      email,
+      role: 'admin',
+      firebaseUID: newFirebaseUser.uid,
+      mongoDBID: savedNewAdmin._id,
+    });
+    const { firebaseUID } = await userCreated.save();
+
+    return res.status(201).json({
+      message: 'User created',
+      data: { firebaseUID, userData: savedNewAdmin },
+    });
+  } catch (error) {
+    Admins.findOneAndRemove(
+      { _id: savedNewAdmin._id },
+      { useFindAndModify: false },
+      () => res.status(400).json({ message: error.toString() })
+    );
+  }
+};
+
 module.exports = {
-  register,
+  registerPostulant,
+  registerPsychologist,
+  registerAdmin,
 };
