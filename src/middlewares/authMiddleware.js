b27ea4a198f2ac376c4e275867e5f5cdb675a96c
Martin Raveglia <ravegliamartin@gmail.com>
2022-01-03 00:42:31 -0300

    MS-88: Add authMiddleware logic

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,6 +1,10 @@
 const firebase = require('../helper/firebase');
 
-const authMiddleware = (req, res, next) => {
+const postulant = 'postulant';
+const psychologist = 'psychologist';
+const admin = 'admin';
+
+const authMiddlewarePostulant = (req, res, next) => {
   const { token } = req.headers;
   if (!token) {
     return res.status(400).json({ message: 'Undefined Token' });
@@ -10,10 +14,56 @@ const authMiddleware = (req, res, next) => {
     .verifyIdToken(token)
     .then((decodedToken) => {
       console.log(decodedToken);
+      const role = decodedToken?.customClaims?.role;
+      if (role === postulant || role === psychologist || role === admin)
+        return next();
+      throw new Error('No User Privileges');
     })
     .catch((error) => {
       res.status(401).json({ message: error.toString() });
     });
 };
 
-module.exports = authMiddleware;
+const authMiddlewarePsychologist = (req, res, next) => {
+  const { token } = req.headers;
+  if (!token) {
+    return res.status(400).json({ message: 'Undefined Token' });
+  }
+  return firebase
+    .auth()
+    .verifyIdToken(token)
+    .then((decodedToken) => {
+      console.log(decodedToken);
+      const role = decodedToken?.customClaims?.role;
+      if (role === psychologist || role === admin) return next();
+      throw new Error('No User Privileges');
+    })
+    .catch((error) => {
+      res.status(401).json({ message: error.toString() });
+    });
+};
+
+const authMiddlewareAdmin = (req, res, next) => {
+  const { token } = req.headers;
+  if (!token) {
+    return res.status(400).json({ message: 'Undefined Token' });
+  }
+  return firebase
+    .auth()
+    .verifyIdToken(token)
+    .then((decodedToken) => {
+      console.log(decodedToken);
+      const role = decodedToken?.customClaims?.role;
+      if (role === admin) return next();
+      throw new Error('No User Privileges');
+    })
+    .catch((error) => {
+      res.status(401).json({ message: error.toString() });
+    });
+};
+
+module.exports = {
+  authMiddlewarePostulant,
+  authMiddlewarePsychologist,
+  authMiddlewareAdmin,
+};
--- src/routes/admins.js
+++ src/routes/admins.js
@@ -1,12 +1,21 @@
-const router = require("express").Router();
-const {findAllAdmins,findOneAdmin,createAdmin,updateAdmin,deleteAdmin} = require("../controllers/admins");
-const {validateBodyContent, validateAdminCreation} = require("../validations/admins")
+const router = require('express').Router();
+const {
+  findAllAdmins,
+  findOneAdmin,
+  createAdmin,
+  updateAdmin,
+  deleteAdmin,
+} = require('../controllers/admins');
+const {
+  validateBodyContent,
+  validateAdminCreation,
+} = require('../validations/admins');
+const { authMiddlewareAdmin } = require('../middlewares/authMiddleware');
 
-
-router.get("/", findAllAdmins);
-router.get("/:id", findOneAdmin);
-router.post("/", validateAdminCreation, createAdmin);
-router.put("/:id", validateBodyContent, updateAdmin);
-router.delete("/:id", deleteAdmin);
+router.get('/', authMiddlewareAdmin, findAllAdmins);
+router.get('/:id', authMiddlewareAdmin, findOneAdmin);
+router.post('/', authMiddlewareAdmin, validateAdminCreation, createAdmin);
+router.put('/:id', authMiddlewareAdmin, validateBodyContent, updateAdmin);
+router.delete('/:id', authMiddlewareAdmin, deleteAdmin);
 
 module.exports = router;
